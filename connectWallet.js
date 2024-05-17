92381372a720ca83aa065b60e41c447cb1897b3b
mac <dobrokhotovsergey@gmail.com>
2024-05-17 23:46:58 +0300

    fix

--- connectWallet.js
+++ connectWallet.js
@@ -1,42 +1,33 @@
-
-
 const tonConnectUI = new TON_CONNECT_UI.TonConnectUI({
     manifestUrl: 'https://farmtest-850d9c786338.herokuapp.com/tonconnect-manifest.json',
-    buttonRootId: 'ton-connect',
+    buttonRootId: 'ton-connect'
 });
 
 tonConnectUI.uiOptions = {
-    twaReturnUrl: 'tg://farmer_2000_Test_bot/farmer_2000_Test_webapp',
+    twaReturnUrl: 't.me//farmer_2000_Test_bot/farmer_2000_Test_webapp'
 };
 
-    console.log('twaReturnUrl is set to:', tonConnectUI.uiOptions);
-
 const unsubscribe = tonConnectUI.onSingleWalletModalStateChange(() => {
-    try {
-        const currentAccount = tonConnectUI.account;
-        const currentIsConnectedStatus = tonConnectUI.connected;
-        if(currentIsConnectedStatus){
-            fetch(middlewareHost + "/user", {
-                method: "POST",
-                body: JSON.stringify({
-                    address: currentAccount.address
-                }),
-                headers: {
-                    "Content-type": "application/json; charset=UTF-8"
-                }
-            }).then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok: ' + response.statusText);
-                }
-                return response.json();
-            }).then(data => {
-                document.getElementById('user-balance').textContent = data.balance;
-            }).catch(error => {
-                console.error('Error during fetch operation:', error.message);
-            });
-        }
-    } catch (error) {
-        console.error('Error in wallet modal state change handler:', error.message);
+    const currentAccount = tonConnectUI.account;
+    const currentIsConnectedStatus = tonConnectUI.connected;
+    if(currentIsConnectedStatus){
+        fetch(middlewareHost + "/user", {
+            method: "POST",
+            body: JSON.stringify({
+                address: currentAccount.address
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        }).then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            return response.json();
+        }).then(data => {
+            userId = data.id;
+            document.getElementById('user-balance').textContent = data.balance;
+        });
     }
-});
 
+});
